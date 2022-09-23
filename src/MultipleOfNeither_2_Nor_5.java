f882d5db5910bbb3d5a5c3e0159fb3a7a6b7e21f
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-23 22:02:23 +0600

    Check a number if it is multiple of neither 2 nor 5

---
+++ src/MultipleOfNeither_2_Nor_5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultipleOfNeither_2_Nor_5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner( System.in );
+        System.out.println("Enter x");
+        int x=scn.nextInt();
+
+        if( x%2 != 0 && x % 5 != 0){
+            System.out.println("it is multiple of neither 2 nor 5!");
+        }
+
+
+    }
+}
