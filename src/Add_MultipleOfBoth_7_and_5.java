4cec7f012a36a882b7286f410da72f9aad5543a7
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-25 21:01:22 +0600

    adds all numbers that are multiples of both 7 and 5 up to 600 (including 600)

---
+++ src/Add_MultipleOfBoth_7_and_5.java
@@ -0,0 +1,16 @@
+public class Add_MultipleOfBoth_7_and_5 {
+    public static void main(String[] args) {
+        int sum=0;
+        int i=0;
+        while (i<=600){
+            if (i%7==0 && i%5==0){
+                sum=sum+i;
+
+            }
+            i++;
+
+        }
+        System.out.println(sum);
+    }
+
+}
