1192ee0228099bae3781922440fd107555d22e05
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-25 20:40:58 +0600

    print 24, 18, 12, 6, 0, -6 this sequence using while loop

---
+++ src/Sequence.java
@@ -0,0 +1,15 @@
+
+public class Sequence {
+    public static void main(String[] args) {
+        int counter = 24;
+        while (counter>=-6){
+            if( counter == -6){
+                System.out.println(counter);
+            }
+            else{
+                System.out.println(counter);
+            }counter =counter-6;
+
+        }
+    }
+}
