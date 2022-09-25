7e873edf5abef20237d8b76fba486bc40a8ed68a
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-25 20:53:42 +0600

    print for a sudden range

---
+++ src/PrintAt_a_suddenRange.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintAt_a_suddenRange {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        System.out.println(" enter your favourite car name:");
+        String Car_name = scn.nextLine();
+        System.out.println(" enter a number:");
+        int number = scn.nextInt();
+        int count=0;
+
+        while (count<number){
+            System.out.println(Car_name);
+            count++;
+
+        }
+
+
+    }
+}
