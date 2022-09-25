b0f5974bbb75ce2dfddaf66f9076b5fc212c316c
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-25 21:18:36 +0600

    displays all the **odd numbers** between 10 and 50

---
+++ src/Display_OddNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Display_OddNumbers {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(" Enter starting number:");
+        int start = scn.nextInt();
+        System.out.println(" Enter starting number:");
+        int end= scn.nextInt();
+        int i=start;
+        while (start<=i && i<=end){
+            if (i%2!=0){
+                System.out.println(i);
+
+            }i++;
+
+
+        }
+
+    }
+}
