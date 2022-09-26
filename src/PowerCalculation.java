5b7a31dffa494c70887436cb8a30f2b29f88a06f
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-26 16:11:19 +0600

    takes a number and prints the value of 10 to the power that number.

---
+++ src/PowerCalculation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int exp = scn.nextInt();
+
+        int result = (int) Math.pow(10,exp);
+        System.out.println(result);
+    }
+}
