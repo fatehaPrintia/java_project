53ad158fccb8086f72b0c04a1dff8154694e977a
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-26 16:47:42 +0600

    asks the user for one number and tells if it is a prime number or not.

---
+++ src/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+        int i = 2;
+        int count=0;
+
+        if (number>1){
+            while (i<number){
+                if (number % i ==0){
+                    count++;
+                }
+                i++;
+
+            }if(count>0){
+                System.out.println("This is not a prime number ");
+
+            }else{
+                System.out.println("This is a prime number ");
+
+            }
+        }
+        else {
+            System.out.println("This is not a prime number ");
+        }
+
+    }
+}
