898e6256a24b96764856a76c338bb4283773c0cf
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-26 20:52:03 +0600

    takes a number from the user and prints its digits from left to right.

---
+++ src/Digit_LeftToRight.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Digit_LeftToRight {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+        int new_number;
+        new_number = number;
+        int count =0;
+        while (number>0){
+            count+=1;
+            number= number/10;
+        }
+        while (new_number>0){
+            int power= (int) Math.pow(10,count-1);
+            int digit = new_number / power;
+            new_number = new_number%power;
+            count--;
+            System.out.println(digit);
+
+        }
+
+
+    }
+}
