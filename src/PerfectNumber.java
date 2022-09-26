a9e1dd24afb731a9164e8e7400b7a67a38a7822e
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-26 16:29:10 +0600

    takes a number and tells if it is a perfect number or not

---
+++ src/PerfectNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+
+        int sum=0;
+        int i =1;
+        while (i<number){
+            if(number%i == 0){
+                sum =sum +i;
+            }i++;
+        }
+        if (sum == number){
+            System.out.println("This is a perfect number " );
+
+        }
+        else {
+            System.out.println("This is not a perfect number " );
+        }
+
+    }
+}
