1a2ce1fefc0dbd8c5df8375c3de7aa3bcbd5720d
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-28 15:39:28 +0600

    prints a rectangle of size M (height) and N(length) using + where M,N will be given as input

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,18 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
 public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int height= scn.nextInt();;
+        int length= scn.nextInt();
+        String str= "+";
+        for (int i=0; i<height;i++){
+            for(int j=0; j<length;j++){
+                System.out.print(str);
+//
+            }System.out.println();
+
+        }
+    }
 }
