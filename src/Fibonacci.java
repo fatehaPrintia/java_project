e1a6517be850343c50f28259eb274fecd890c3a8
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-28 16:44:54 +0600

    program that prints all the fibonacci numbers from 0 to N where N will be given as input by the user.

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int range = scn.nextInt();
+        int fib=0;
+        int sum=1;
+        System.out.print(fib+" "+sum+" ");
+        while(fib+sum <=range){
+            int temp = fib+sum;
+            System.out.print(temp);
+            System.out.print(" ");
+            fib=sum;
+            sum=temp;
+        }
+    }
 }
