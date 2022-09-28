a96fcff4cbac35e69b381d1b7ee4f0deaae0519d
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-09-28 16:20:46 +0600

    prints a right angled triangle of height N using incrementing numbers where N will be given as input.

--- src/RightAngle_Triangle.java
+++ src/RightAngle_Triangle.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class RightAngle_Triangle {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int height = scn.nextInt();
-        ;
+        int N = scn.nextInt();
 
-        for (int i = 0; i <=height; i++) {
+
+        for (int i = 0; i <=N; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
 
