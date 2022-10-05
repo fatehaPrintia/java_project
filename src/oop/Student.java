25434f607564b356071847055864d72f603a55fc
iamFatehaJannatPrintia <fatehaprintia@gmail.com>
2022-10-05 19:07:08 +0600

    lab01

---
+++ src/oop/Student.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Student {
+    public String name;
+    public  String Student_id;
+    public  String email;
+
+    public void print(){
+        System.out.println(name);
+        System.out.println(Student_id);
+        System.out.print(email);
+
+    }
+
+
+}
---
+++ src/oop/test.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class test {
+    public static void main(String[] args) {
+        Student a = new Student();
+        a.name="printia";
+        a.Student_id="20301357";
+        a.email="fatheaprintia@gmail.com";
+        a.print();
+
+    }
+}
